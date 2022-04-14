6814c66d4adc175b597c44163081e51642a4053c
GeorgeCiachir <george.ciachir@yahoo.com>
2022-04-14 07:42:28 +0300

    deserialize into an EventGridEvent

--- src/main/java/com/georgeciachir/functions/EventGridTriggerFunction.java
+++ src/main/java/com/georgeciachir/functions/EventGridTriggerFunction.java
@@ -1,17 +1,24 @@
 package com.georgeciachir.functions;
 
-import com.azure.messaging.eventgrid.EventGridEvent;
+//import com.azure.messaging.eventgrid.EventGridEvent;
+import com.azure.messaging.eventgrid.implementation.models.EventGridEvent;
 import com.microsoft.azure.functions.ExecutionContext;
 import com.microsoft.azure.functions.annotation.EventGridTrigger;
 import com.microsoft.azure.functions.annotation.FunctionName;
 
+import java.util.List;
+
 public class EventGridTriggerFunction {
 
     @FunctionName("event-grid-function")
-    public void run(@EventGridTrigger(name = "eventGridEvent") EventGridEvent message,
+    public void run(@EventGridTrigger(name = "eventGridEvent") EventGridEvent event,
                     final ExecutionContext context) {
         context.getLogger().info("Java Event Grid trigger function executed.");
-        context.getLogger().info(message.getEventType());
-        context.getLogger().info(message.getData().toString());
+
+//        List<EventGridEvent> events = EventGridEvent.fromString(message);
+//        EventGridEvent event = events.get(0);
+
+        context.getLogger().info(event.getEventType());
+        context.getLogger().info(event.getData().toString());
     }
 }
